b831cfbe9922dcbad2c839aa9e6af40c93849ed3
leonardolim78 <leonardolim178@gmail.com>
2021-12-20 21:41:18 +0700

    Add routes

--- app.js
+++ app.js
@@ -7,6 +7,19 @@ const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
 
+// Routes
+const homeRoute = require('./routes/index');
+const aboutRoute = require('./routes/about');
+const productsRoute = require('./routes/products');
+const receiptsRoute = require('./routes/receipts');
+const usersRoute = require('./routes/users');
+
+app.use('/', homeRoute);
+app.use('/about', aboutRoute);
+app.use('/products', productsRoute);
+app.use('/receipts', receiptsRoute);
+app.use('/users', usersRoute);
+
 // Server
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
